83237c0910ca2c89cd79d94e3686ea8a7c55d13f
Frederic-h4800 <tf.huang288@gmail.com>
2022-06-14 10:01:26 -0400

    Pad Computer code started

--- PadComputer/src/main.cpp
+++ PadComputer/src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+
+ int servoPin = A0;
+  Servo launchClamp;
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+ 
+  launchClamp.attach(servoPin);
+  launchClamp.write(0);
+  delay(1000);
+  String command = Serial.readString();
+
+  if(command == "Send It!"){
+    delay(200);
+    launchClamp.write(180);
+    delay(500);
+  }
+
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
